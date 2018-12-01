2b378c14f0bf08f5c00129d4eff1a61c2e1fe860
郭子豪 <jessica520702@gmail.com>
2018-12-01 19:24:51 +0800

    first commit

---
+++ DS測試版.cpp
@@ -0,0 +1,263 @@
+#include <cstdio>
+#include <iostream>
+#include <queue>
+#include <stack> 
+#include <cstring>
+#include <string>
+#include <fstream> 
+using namespace std;
+const int MAX_N = 1050;
+const int MAX_M = 1050;
+const int INF = 1000000;
+int steps = 0;
+int ans_x[MAX_N];
+int ans_y[MAX_M];
+int rows, cols;
+typedef pair<int, int> P;
+char maze[MAX_N][MAX_M];
+int Rx, Ry; ///location of R
+bool flag[MAX_N][MAX_M]={0};
+
+int k=0;
+int s=0;
+int pox[MAX_N];
+int poy[MAX_M];
+int sox[MAX_N];
+int soy[MAX_M];
+	
+int d[MAX_N][MAX_M];///the distance of R to the point
+int dx[4] = { 1,0,-1,0 }, dy[4] = { 0,1,0,-1 }; ///the displacement
+
+bool flag2[4]={0};
+
+int dir[4][2]={-1,0,1,0,0,-1,0,1};
+struct node
+{
+	int x,y;
+	node *last;
+	node(int xx,int yy){x=xx;y=yy;};
+	node(){};
+};
+
+void bfs3(int sx,int sy,int ex,int ey)
+{
+	node *n;
+	queue<node*> Q;
+	node *start = new node(sx,sy);
+	start->last=NULL;
+	Q.push(start);
+	while (Q.size())
+	{
+		n = Q.front();
+		Q.pop();
+
+		if(n->x == ex&&n->y == ey)
+			break;
+
+		for (int i = 0; i < 4; i++)
+		{
+			int nx = n->x + dx[i];
+			int ny = n->y + dy[i];
+			if (0 <= nx&&nx < rows
+				&& 0 <= ny&&ny < cols
+				&&d[nx][ny] != INF
+				&&d[nx][ny] != 0
+				&&(d[nx][ny]-d[n->x][n->y]==-1||d[nx][ny]-d[n->x][n->y]==1))
+			{
+				if(i==0){node *right = new node(nx,ny); right->last=n; Q.push(right);}
+				if(i==1){node *down = new node(nx,ny); down->last=n; Q.push(down);}
+				if(i==2){node *left = new node(nx,ny); left->last=n; Q.push(left);}
+				if(i==3){node *up = new node(nx,ny); up->last=n; Q.push(up);}
+			}
+		}
+	}
+	node *current = n;
+	while(current!=NULL)
+	{
+		sox[s]=current->x;
+		soy[s]=current->y;
+		current = current->last;
+		s++;
+	}
+	return;
+}
+
+void bfs2(int ex,int ey)
+{
+	if(flag[ex][ey]==1)return;
+	node *n;
+	queue<node*> Q;
+	node *start = new node(Rx,Ry);
+	start->last=NULL;
+	Q.push(start);
+	while (Q.size())
+	{
+		n = Q.front();
+		Q.pop();
+
+		if(n->x == ex&&n->y == ey)
+			break;
+
+		for (int i = 0; i < 4; i++)
+		{
+			int nx = n->x + dx[i];
+			int ny = n->y + dy[i];
+			if (0 <= nx&&nx < rows
+				&& 0 <= ny&&ny < cols
+				&&d[nx][ny] != INF
+				&&d[nx][ny]>d[n->x][n->y])
+			{
+				if(i==0){node *right = new node(nx,ny); right->last=n; Q.push(right);}
+				if(i==1){node *down = new node(nx,ny); down->last=n; Q.push(down);}
+				if(i==2){node *left = new node(nx,ny); left->last=n; Q.push(left);}
+				if(i==3){node *up = new node(nx,ny); up->last=n; Q.push(up);}
+			}
+		}
+	}
+	node *current = n;
+	while(current!=NULL)
+	{
+		//cout<<current->x<<" "<<current->y<<endl;
+		flag[current->x][current->y]=1;
+		pox[k]=current->x;
+		poy[k]=current->y;
+		current = current->last;
+		k++;
+	}
+	return;
+}
+
+void bfs()
+{
+	queue<P> Q;
+	for (int i = 0; i < rows; i++)
+    {
+		for (int j = 0; j < cols; j++)
+        {
+			d[i][j] = INF;
+        }
+    }
+	Q.push(P(Rx, Ry));
+	d[Rx][Ry] = 0; ///start from R, distance = 0
+
+	while (Q.size())
+	{
+		P p = Q.front();
+		Q.pop();
+		if(maze[p.first+1][p.second] != '0' &&
+           maze[p.first-1][p.second] != '0' &&
+           maze[p.first][p.second+1] != '0' &&
+           maze[p.first][p.second-1] != '0') continue;
+
+		for (int i = 0; i < 4; i++)
+		{
+			int nx = p.first + dx[i];
+			int ny = p.second + dy[i];
+			if (0 <= nx&&nx < rows
+				&& 0 <= ny&&ny < cols
+				&&maze[nx][ny] == '0')
+			{
+				Q.push(P(nx, ny));
+				maze[nx][ny] = '2';
+				steps++;
+				ans_x[steps] = nx;
+				ans_y[steps] = ny;
+				d[nx][ny] = d[p.first][p.second] + 1;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int battery;
+	cin >> rows >> cols >> battery;
+	for(int i = 0; i < rows; ++i)
+    {
+        for(int j = 0; j < cols; ++j)
+        {
+            cin >> maze[i][j];
+        }
+    }
+	for (int i = 0; i < rows; i++)
+    {
+		for (int j = 0; j < cols; j++)
+		{
+			if (maze[i][j] == 'R')
+			{
+				Rx = i;
+				Ry = j;
+			}
+		}
+    }
+	
+	bfs();
+	
+	cout << steps << endl;
+
+	for(int i = 1; i <= steps; ++i)
+    {
+        if(i == steps) cout << ans_x[i] << " " << ans_y[i]<<endl;
+        else cout << ans_x[i] << " " << ans_y[i] << endl;
+    }
+    for(int i = 0;i < rows ; ++i )
+		for(int j = 0;j < cols ; ++j )
+		{
+			if(j!=cols-1)
+				cout<<d[i][j]<<" ";
+			else
+				cout<<d[i][j]<<endl;
+		}
+
+	for(int i = steps; i >= 1; --i)
+	{
+		bfs2(ans_x[i],ans_y[i]);
+	}
+
+	stack<int> sx;
+	stack<int> sy;
+	int fx;
+	int fy;
+	for(int i=0;i<k;i++)
+	{
+		if(pox[k-i-1]==1&&poy[k-i-1]==1)
+		{
+			if(i==0){fx=pox[k-i-2];fy=poy[k-i-2];}
+			else
+			{
+			sx.pop();sy.pop();
+			while(!sx.empty())
+			{
+				cout<<sx.top()<<" "<<sy.top()<<endl;
+				sx.pop();sy.pop();
+			}
+				cout<<Rx<<" "<<Ry<<endl;
+				
+			}	
+			if(fx!=pox[k-i-2]||fy!=poy[k-i-2])
+			{
+				bfs3(fx,fy,pox[k-i-2],poy[k-i-2]);
+				for(int j=0;j<s;j++)
+				{
+					cout<<" "<<sox[s-j-1]<<" "<<soy[s-j-1]<<endl;
+				}
+				cout<<Rx<<" "<<Ry<<endl;
+				fx=pox[k-i-2];fy=poy[k-i-2];
+			}					  	
+		}	
+		else
+		{
+			cout<<pox[k-i-1]<<" "<<poy[k-i-1]<<endl;
+			sx.push(pox[k-i-1]);
+			sy.push(poy[k-i-1]);		
+		}
+		s=0;
+	}
+	sx.pop();sy.pop();
+	while(!sx.empty())
+	{
+		cout<<sx.top()<<" "<<sy.top()<<endl;
+		sx.pop();sy.pop();
+	}
+	cout<<Rx<<" "<<Ry;	
+}
